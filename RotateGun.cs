181e9fa03dc1eb06ce0d9ed5b545d9e16d5d5708
qqq2qqq2 <119753244+qqq2qqq2@users.noreply.github.com>
2024-05-27 10:24:26 +0330

    Update RotateGun.cs

--- RotateGun.cs
+++ RotateGun.cs
@@ -5,7 +5,7 @@ public class RotateGun : MonoBehaviour {
     public GrapplingGun grappling;
 
     private Quaternion desiredRotation;
-    private float rotationSpeed = 5f;
+    private float rotationSpeed = 5d;
 
     void Update() {
         if (!grappling.IsGrappling()) {
